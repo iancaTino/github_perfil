b62c2a99484e2376081c65bcbf16bd5a298bcd5c
ianca <iancalaurentino@gmai.com>
2026-02-01 12:15:36 -0300

    corrige caminho do componente ReposList

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
-
 import Perfil from "./components/Perfil";
 import ReposList from "./components/Perfil/ReposList";
 
 function App() {
-  const [nomeUsuario, setNomeUsuario] = useState('');
+  const [nomeUsuario, setNomeUsuario] = useState("");
 
   return (
-    <div className="container">
-      <h1 className="titulo">GitHub</h1>
+    <div className="app">
+      <h1 className="title">GitHub Profile Finder</h1>
+      <p className="subtitle">
+        Digite um usuário do GitHub e veja os repositórios ✨
+      </p>
 
-      <input
-        type="text"
-        placeholder="Digite o nome do usuário e pressione Enter"
-        onKeyDown={(e) => {
-          if (e.key === "Enter") setNomeUsuario(e.target.value);
-        }}
-        className="input"
-      />
+      <div className="search">
+        <input
+          type="text"
+          placeholder="ex: iancaTino"
+          value={nomeUsuario}
+          onChange={(e) => setNomeUsuario(e.target.value.trim())}
+        />
+      </div>
 
-      {nomeUsuario.length > 4 && (
-        <>
+      {nomeUsuario.length > 3 && (
+        <div className="content">
           <Perfil nomeUsuario={nomeUsuario} />
           <ReposList nomeUsuario={nomeUsuario} />
-        </>
+        </div>
       )}
     </div>
   );
 }
 
 export default App;
+
+
+{/* {formularioEstaVisivel && (
+        <Formulario />
+      )}
+      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">Toggle form</button> */}
